e649f8de160e20133d5e5c6e7126e23712d3a8cb
kmimaki <kmimaki@netprotections.co.jp>
2021-08-10 17:49:15 +0900

    first commit

---
+++ src/main/java/jp/co/netprotections/CalculationApiApplication.java
@@ -0,0 +1,13 @@
+package jp.co.netprotections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/netprotections/CalculationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.netprotections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
