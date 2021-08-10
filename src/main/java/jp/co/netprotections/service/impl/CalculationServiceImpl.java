0635f0de2873b1a8cf3ec81538898bae65678513
mima-k <mimakouhei@gmail,com>
2021-08-11 03:18:05 +0900

    ビシネスロジック書いた

--- src/main/java/jp/co/netprotections/service/impl/CalculationServiceImpl.java
+++ src/main/java/jp/co/netprotections/service/impl/CalculationServiceImpl.java
@@ -6,6 +6,11 @@ import jp.co.netprotections.dto.ValueRequestDto;
 import jp.co.netprotections.dto.ValueResponseDto;
 import jp.co.netprotections.service.CalculationService;
 
-public class CalculationServiceImpl {
-
+public class CalculationServiceImpl implements CalculationService {
+	public ValueResponseDto calculate(ValueRequestDto request) {
+		ValueResponseDto response = new ValueResponseDto();
+		int square = request.getValue() * request.getValue();
+		response.setValue(square);
+		return response;
+	}
 }
